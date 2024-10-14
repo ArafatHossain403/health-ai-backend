42edbb4255c9effa5a5b053a63f1ec3e7930f85f
ArafatHossain403 <arafatnubt@gmail.com>
2024-10-15 00:16:14 +0600

    main change

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-//require('dotenv').config();
 import * as dotenv from 'dotenv';
 
 async function bootstrap() {
@@ -8,7 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  const port = process.env.APP_PORT || 3000;
+  // Use the dynamic PORT environment variable provided by Render
+  const port = process.env.PORT || 3000; // Default to 3000 if PORT is not set
 
   await app.listen(port);
   console.log(`app started on port: ${port}`);
